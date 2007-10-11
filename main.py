ba02af929bd09d88cb19e29cc6ea9c310f871a45
John (J5) Palmieri <johnp@redhat.com>
2007-10-11 17:47:52 -0400

    print out service info for debugging

--- main.py
+++ main.py
@@ -1,13 +1,25 @@
 import sys
 import gtk
+import gobject 
 
 import dbus_introspector
 from dbus_introspector import BusWatch
 
 def main(args):
+    global session_bus_watch
     session_bus_watch = BusWatch(dbus_introspector.SESSION_BUS)
+
+def print_services():
+    global session_bus_watch
+    print '\n*************** '
+    for key in session_bus_watch.services.keys():
+        print str(session_bus_watch.services[key])
+    print '\n'
+
+    return True
 
 if __name__ == "__main__":
     main(sys.argv)
 
+    gobject.timeout_add(5000, print_services)
     gtk.main()
