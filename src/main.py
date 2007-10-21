366d24d69642e80e4f33852d52a8ed35d78f3ac3
John (J5) Palmieri <johnp@redhat.com>
2007-10-21 14:50:08 -0400

    integrate gazpacho created glade files into the UI

--- src/main.py
+++ src/main.py
@@ -1,23 +1,44 @@
+import os
 import sys
 import gtk
+import gtk.glade
 import gobject 
 import _ui
 
 import dbus_introspector
 from dbus_introspector import BusWatch
 
+glade_dir = os.environ['GLADE_DIR']
+glade_file = glade_dir + '/dfeet.glade' 
+
 def main(args):
     global session_bus_watch
+
     session_bus_watch = BusWatch(dbus_introspector.SESSION_BUS)
-    main_window = gtk.Window(gtk.WINDOW_TOPLEVEL)
-    main_window.set_size_request(300, 200)
+    glade_xml = gtk.glade.XML(glade_file)
+
+    main_window = glade_xml.get_widget('window1')
     main_window.connect('destroy',gtk.main_quit)
+
+    # TODO: make a UI class for each tab view
     session_services_view = _ui.ServiceView(session_bus_watch)
     session_services_view.show()
-    scroll = gtk.ScrolledWindow()
-    scroll.add(session_services_view)
-    scroll.show()
-    main_window.add(scroll)
+    service_list_scroll = gtk.ScrolledWindow()
+    service_list_scroll.add(session_services_view)
+    service_list_scroll.show()
+    
+    details_scroll = gtk.ScrolledWindow()
+    details_scroll.show()
+
+    pane = gtk.HPaned()
+    pane.pack1(service_list_scroll)
+    pane.pack2(details_scroll)
+    pane.set_position(300)
+    pane.show()
+    
+    notebook = glade_xml.get_widget('display_notebook')
+    notebook.append_page(pane, gtk.Label('Session Bus'))
+     
     main_window.show()
 
 def print_services():
